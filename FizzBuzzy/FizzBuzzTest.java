f8ba38e8a85f887a510147e9ffa71113fe601a27
Sandra Opone <31860053+Sandraopone1@users.noreply.github.com>
2018-04-13 20:58:12 -0400

    Add files via upload

---
+++ FizzBuzzy/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest{
+	public static void main(String[] args){
+		FizzBuzz val = new FizzBuzz();
+		String result = val.FizzBuzzResult(3);
+		System.out.println(result);
+	}
+}
+
+
